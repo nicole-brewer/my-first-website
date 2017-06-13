5d55340b8905e2c5b2fbfd8376d938854896bac9
nicole-brewer <brewer36@purdue.edu>
2017-06-13 09:56:54 -0400

    Added files that weren't beeing tracked... whoops

---
+++ footer.php
@@ -0,0 +1,8 @@
+
+<footer>
+		<div id="copyright">Copyright nicole-brewer.com. All rights reserved.</div>
+		<div id="pro-icons">
+			<a href="mailto:brewer36@purdue.edu" class="menu-item social"><i class="fa fa-envelope"></i></a>
+			<a href="https://github.com/nicole-brewer" class="menu-item social"><i class="fa fa-github"></i></a>
+			<a href="https://www.linkedin.com/in/nicole-brewer" class="menu-item social"><i class="fa fa-linkedin"></i></a></div>
+</footer>
---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Nicole Brewer</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="styles/blog/index.css">
+	<link rel="stylesheet" href="styles/font-awesome/css/font-awesome.min.css">
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300|Source+Sans+Pro:300" rel="stylesheet">
+	<script type="text/javascript" src="scripts/skrollr.min.js"></script>
+  </head>
+  <body>
+  <?php include("header.php"); ?>
+  <p>Main body content!</p>
+  <?php include("footer.php"); ?>
+  </body>
+</html>
