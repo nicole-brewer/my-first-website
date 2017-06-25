7dd7fc478007546527da6215595447b9d58db025
nicole-brewer <brewer36@purdue.edu>
2017-06-25 15:32:31 -0400

    Added a few folders I missed in the last commit

---
+++ web/index.php
@@ -0,0 +1,7 @@
+
+<?php
+	define('TITLE', 'Home | nicole-brewer.com');
+  include('../includes/header.php');
+?>
+  <p>Main Content :)</p>
+  <?php include('../includes/footer.php'); ?>
---
+++ web/rssfeed.php
@@ -0,0 +1,23 @@
+<?xml version="1.0" encoding="utf-8"?>
+<rss version="2.0">
+<channel>
+<title>nicole-brewer.com</title>
+<link>http://nicole-brewer.com/blog</link>
+<description>I need to make one of these!</description>
+
+<item>
+  <title>Entry Title</title>
+  <link>Link to the entry</link>
+  <guid>http://example.com/item/123</guid>
+  <pubDate>Sat, 9 Jan 2010 16:23:41 GMT</pubDate>
+  <description>[CDATA[ This is the description. ]]</description>
+</item>
+
+</channel>
+</rss>
+
+notes:
+Use php to list all posts
+
+add the line AddType application/x-httpd-php .xml
+in .htaccess to make Apache treat XML files as PHP files
